f02e8c690214e4857a853afc7120b2f89928f1c3
Andriele Lange <andrielebarcelange@gmail.com>
2024-11-15 17:03:38 -0300

    GET lists

---
+++ src/main/java/com/andriele/dslist/dto/GameListDTO.java
@@ -0,0 +1,25 @@
+package com.andriele.dslist.dto;
+
+import com.andriele.dslist.entities.GameList;
+
+public class GameListDTO {
+    private Long id;
+    private String name;
+
+
+    public GameListDTO() {
+    }
+
+    public GameListDTO(GameList entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/andriele/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.andriele.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.andriele.dslist.dto.GameListDTO;
+import com.andriele.dslist.entities.GameList;
+import com.andriele.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll(){
+        List<GameList> result = gameListRepository.findAll();
+        return result.stream().map(x -> new GameListDTO(x)).toList();
+    }
+}
